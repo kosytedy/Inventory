146c8d391e117d73a7fd8e1e395790f06625f548
Teddy <kosytedy@yahoo.com>
2021-05-12 08:55:22 +0100

    Created Product model

---
+++ Inventory/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public virtual ICollection<Sales> ProductSold { get; set; }
+
+        public Product()
+        {
+        }
+    }
+}
