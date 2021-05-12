345f38e4185a66b5de03cbbce48bd06878989bb7
Teddy <kosytedy@yahoo.com>
2021-05-12 08:50:52 +0100

    Created Sales model

---
+++ Inventory/Models/Sales.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inventory.Models
+{
+    public class Sales
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int StoreId { get; set; }
+
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateSold { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public virtual Store Store { get; set; }
+
+        public Sales()
+        {
+        }
+    }
+}
