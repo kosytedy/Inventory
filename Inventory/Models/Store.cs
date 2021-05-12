cdb04600ed96bd8d5bc2d4faf2ce264cd8901d79
Teddy <kosytedy@yahoo.com>
2021-05-12 09:00:33 +0100

    Added annotation to Address property

---
+++ Inventory/Models/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public virtual ICollection<Sales> ProductSold { get; set; }
+
+        public Store()
+        {
+        }
+    }
+}
