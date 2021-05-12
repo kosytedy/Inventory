61f2b18f9b90fec1ae4f996dc522bb019de70e68
Teddy <kosytedy@yahoo.com>
2021-05-12 08:24:49 +0100

    Created Customer model

--- Inventory/Models/Customer.cs
+++ Inventory/Models/Customer.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Inventory.Models
 {
     public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public virtual ICollection<Sales> ProductSold { get; set; }
+
         public Customer()
         {
         }
+
     }
 }
