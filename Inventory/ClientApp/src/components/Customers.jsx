0825216e0f29d388b4a4e01442da152cf77cb3d0
Teddy <kosytedy@yahoo.com>
2021-05-13 10:32:13 +0100

    Created Customers component

---
+++ Inventory/ClientApp/src/components/Customers.jsx
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from 'react';
+import CustomerTable from './tables/CustomerTable';
+import axios from 'axios';
+import { Button } from 'semantic-ui-react'
+
+class Customers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  
+            customers: [],
+        }
+    }
+
+    componentDidMount(){
+        this.getCustomers();
+    }
+
+    render() { 
+        const { customers } = this.state;
+        return (  
+            <Fragment>
+                <h3>Customers</h3>
+                <Button content='New Customer' icon='plus' labelPosition='left' primary />
+                <CustomerTable customers={customers} />
+            </Fragment>
+        );
+    }
+
+    getCustomers = () => {
+        axios.get('/api/Customers')
+            .then(({data}) => {
+                this.setState({
+                    customers: data,
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
+export default Customers;
