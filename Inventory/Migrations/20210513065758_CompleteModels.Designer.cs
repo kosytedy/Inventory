9e4afaa94909bb67cbcd89b1a4dd2e135e987981
Teddy <kosytedy@yahoo.com>
2021-05-13 08:00:23 +0100

    Updated Customer DB

--- Inventory/Migrations/20210513065758_CompleteModels.Designer.cs
+++ Inventory/Migrations/20210513065758_CompleteModels.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Inventory.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Inventory.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210513065758_CompleteModels")]
+    partial class CompleteModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Inventory/Migrations/20210513065758_CompleteModels.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Inventory.Migrations
+{
+    public partial class CompleteModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Customers_CustomerId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers");
+
+            migrationBuilder.RenameTable(
+                name: "Customers",
+                newName: "Customer");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customer",
+                table: "Customer",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Customer_CustomerId",
+                table: "Sales",
+                column: "CustomerId",
+                principalTable: "Customer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Customer_CustomerId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customer",
+                table: "Customer");
+
+            migrationBuilder.RenameTable(
+                name: "Customer",
+                newName: "Customers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Customers_CustomerId",
+                table: "Sales",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
